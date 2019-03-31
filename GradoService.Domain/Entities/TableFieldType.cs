e6b1f6969e0b1c49d20899d2f5b3ef27715ba4ef
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-04-01 00:41:51 +0300

    Implemented MetadataDbContext with initial 3 classes

---
+++ GradoService.Domain/Entities/TableFieldType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradoService.Domain.Entities
+{
+    public class TableFieldType
+    {
+        public TableFieldType()
+        {
+            FieldInfos = new HashSet<TableFieldInfo>();
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PresentationName { get; set; }
+
+        public ICollection<TableFieldInfo> FieldInfos { get; private set; }
+    }
+}
---
+++ GradoService.Persistence/Configuration/TableInfoConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GradoService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GradoService.Persistence.Configuration
+{
+    public class TableInfoConfiguration : IEntityTypeConfiguration<TableInfo>
+    {
+        public void Configure(EntityTypeBuilder<TableInfo> builder)
+        {
+            builder.ToTable("table_info", "sys_scheme");
+
+            builder.HasKey(k => k.Id);
+
+            builder.Property(e => e.Id).HasColumnName("id");
+
+            builder.Property(e => e.SchemeMap).HasColumnName("scheme_name");
+
+            builder.Property(e => e.Name).HasColumnName("name_db");
+
+            builder.Property(e => e.PresentationName).HasColumnName("name_map");
+        }
+    }
+}

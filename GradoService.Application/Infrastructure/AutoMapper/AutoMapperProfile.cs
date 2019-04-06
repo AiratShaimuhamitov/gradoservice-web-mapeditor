380e93592a0ea10658fd959ceceb5221dd17676f
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-04-06 17:51:48 +0300

    Added Automapper, profile initialization

---
+++ GradoService.Application/Infrastructure/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace GradoService.Application.Infrastructure.AutoMapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            LoadConvertors();
+            LoadStandardMappings();
+            LoadCustomMappings();
+        }
+
+        private void LoadConvertors()
+        {
+
+        }
+
+        private void LoadStandardMappings()
+        {
+            var mapsFrom = MappingProfileHelper.LoadStandardMappings(Assembly.GetExecutingAssembly());
+
+            foreach(var map in mapsFrom)
+            {
+                CreateMap(map.Source, map.Destination).ReverseMap();
+            }
+        }
+
+        private void LoadCustomMappings()
+        {
+            var mapsFrom = MappingProfileHelper.LoadCustomMappings(Assembly.GetExecutingAssembly());
+
+            foreach(var map in mapsFrom)
+            {
+                map.CreateMappings(this);
+            }
+        }
+    }
+}

f7365796862349f8dac42277988661e94c95c1bb
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-04-08 00:00:13 +0300

    Added domain entities for generic table in database

---
+++ GradoService.Domain/Entities/Table/Field.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradoService.Domain.Entities.Table
+{
+    public class Field
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
---
+++ GradoService.Domain/Entities/Table/Row.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradoService.Domain.Entities.Table
+{
+    public class Row
+    {
+        public int TableId { get; set; }
+
+        public Dictionary<Field, object> Data { get; set; }
+    }
+}

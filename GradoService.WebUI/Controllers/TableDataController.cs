dbbc1b917ccf55f618b14c2e39a2450c9780cdff
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-04-10 20:15:44 +0300

    implemented getting table data control in controller

--- GradoService.WebUI/Controllers/TableDataController.cs
+++ GradoService.WebUI/Controllers/TableDataController.cs
@@ -2,33 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GradoService.Application.Tables.Queries.GetTableData;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GradoService.WebUI.Controllers
 {
     [Route("api/table/{id}/data")]
     [ApiController]
-    public class TableDataController
+    public class TableDataController : BaseController
     {
         [HttpGet]
-        public JsonResult Get(int id)
+        public async Task<string> Get(int id)
+        {
+            var table = await Mediator.Send(new GetTableDataQuery { TableId = id });
+
+            return JsonConvert.SerializeObject(table, Formatting.Indented);
+        }
+
+        [HttpGet("row")]
+        public JsonResult GetRow(int id)
         {
             throw new NotImplementedException();
         }
 
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
         public JsonResult UpdateRow(int id)
         {
             throw new NotImplementedException();
         }
 
         [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
         public JsonResult DeleteRow(int id)
         {
             throw new NotImplementedException();
         }
 
         [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesDefaultResponseType]
         public JsonResult InsertRow(int id)
         {
             throw new NotImplementedException();

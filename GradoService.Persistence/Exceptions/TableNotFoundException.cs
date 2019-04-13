6c1cb810d61c95fc04f8e0cf5f44e7eb4f695b4c
Airat Shaimuhamitov <shaimuhamitov@gmail.com>
2019-04-13 14:39:28 +0300

    added exceptions models to persistence layer

---
+++ GradoService.Persistence/Exceptions/TableNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradoService.Persistence.Exceptions
+{
+    public class TableNotFoundException : Exception
+    {
+        public int TableId { get; set; }
+
+        public TableNotFoundException(int id)
+            : base($"Table id = \'{id}\' not found.")
+        {
+            TableId = id;
+        }
+    }
+}
